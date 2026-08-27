1cc9597aeab5baab1f8bb46d9115bf2767732292
thierryseba <109303547+thierryseba@users.noreply.github.com>
2026-08-27 14:23:09 +0000

    Add new  exo2

--- Exo-Asset.py
+++ Exo-Asset.py
@@ -1,34 +1,36 @@
 from datetime import datetime
-from airflow.sdk import DAG, Asset
+from airflow.sdk import DAG, Asset, task
 from airflow.operators.bash import BashOperator
 
 mon_fichier = Asset("file:///tmp/mon_asset.txt")
 
 with DAG(
     dag_id="producteur_asset_bash",
-    start_date=datetime (2026, 8, 27, tz='UTC'),
+    start_date=datetime (2026, 8, 27),
     schedule=None,
     catchup=False,
-    tags=["dataset"],
+    tags=["dataset_generate"],
 
-) as dag:
-    @task(outlets=[mon_fichier])
+) as dag_producteur:
+    
     tache1 = BashOperator(
         task_id="produire_fichier",
         bash_command= "echo 'hello' > /tmp/mon_asset.txt'",
+        outlets=[mon_fichier]
     )
 
 with DAG(
     dag_id="consommateur_asset_bash",
-    start_date=datetime (2026, 8, 27, tz=3Zurope/Paris'),
+    start_date=datetime (2026, 8, 27),
     schedule=[mon_fichier],
     catchup=False,
     tags=["dataset_consume"],
 
-) as dag:
+) as dag_consommateur:
     tache2 = BashOperator(
         task_id="lire_fichier",
         bash_command= "cat /tmp/mon_asset.txt",
+        
 
     )
 

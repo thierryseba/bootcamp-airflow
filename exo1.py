ac9c870be40e76a441c4673917b81513c7bc8fe8
thierryseba <109303547+thierryseba@users.noreply.github.com>
2026-08-27 11:49:47 +0000

    Add new DAG

--- exo1.py
+++ exo1.py
@@ -8,15 +8,15 @@ def _task_a():
 
 with DAG(
     dag_id='hello_airflow',
-    start_date=datetime(2026, 8, 27)
+    start_date=datetime(2026, 8, 27),
     schedule='@daily',
     catchup=False
 ):
 
-task_a = BashOperator(
-    task_di='task_a',
+   task_a = BashOperator(
+    task_id='task_a',
     bash_callable=_task_a,
-)
+   )
 
-task_a
+   task_a
     

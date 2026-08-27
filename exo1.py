1933a74ea06d6c083b233f3cecf8ccd00f8cc296
thierryseba <109303547+thierryseba@users.noreply.github.com>
2026-08-27 11:41:25 +0000

    Add new DAG exo1

---
+++ exo1.py
@@ -0,0 +1,22 @@
+from airflow import dag
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+def _task_a():
+    print("Bonjour")
+
+with DAG(
+    dag_id='hello_airflow',
+    start_date=datetime(2026, 8, 27)
+    schedule='@daily',
+    catchup=False
+):
+
+task_a = BashOperator(
+    task_di='task_a',
+    bash_callable=_task_a,
+)
+
+task_a
+    
